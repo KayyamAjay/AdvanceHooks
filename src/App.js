241fd29009c608907ea40e80c8ad88a38e64d0ba
Ajay Kayyam <saiajay220@gmail.com>
2021-11-15 13:56:52 +0530

    using useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,25 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect( ()=>{
+    const itemStored = localStorage.getItem('isLoggedIn');
+    if(itemStored === '1'){
+      setIsLoggedIn(true);
+    }
+  },[]);//useEffect need two things one is a function which can
+  //be arrow function and other dependency array so that this useEffect runs when
+  // ever those get changed.
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn','1');
+
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
